d8edf16b96962cd4706f3a56ce1306dba5453c3c
PierminatorQ <pierrequintero@gmail.com>
2020-09-04 20:15:21 -0400

    devise added, list controller added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :lists 
+  root to: "lists#index"
+
   devise_for :users
   #root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
